129f6f22fc239b7111727ba149ca4ea4660289e6
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2018-04-17 22:40:24 +0300

    add examples for bigobenchmark and autoplot.bigobenchmark

--- R/bigobenchmark.R
+++ R/bigobenchmark.R
@@ -5,6 +5,8 @@ is.bigobenchmark <- function(x) {
 
 #' bigobenchmark runner
 #'
+#' @importFrom dplyr bind_rows
+#'
 #' @param ... Expressions to benchmark. Passed to \code{microbenchmark}.
 #' @param args List of arguments to run functions on.
 #' @param list List of unevaluated expression to benchmark. Passed to \code{microbenchmark}.
@@ -12,10 +14,13 @@ is.bigobenchmark <- function(x) {
 #' @param check Function to check if the expressions are equal. By default \code{NULL} which omits the check. Passed to \code{microbenchmark}.
 #' @param control List of control arguments. See Details section for \code{microbenchmark}. Passed to \code{microbenchmark}.
 #'
-#' @return
+#' @return An object of class \code{bigobenchmark}.
 #' @export
 #'
 #' @examples
+#' # Measure difference between linear and quadratic functions
+#' bench <- bigobenchmark(1:n, for(i in 1:n) for(i in 1:n) 1:n, args=seq(from=1, to=100, length.out = 50))
+#' bench
 #'
 #' @author Andrew Prokhorenkov
 bigobenchmark <- function(...,
@@ -36,12 +41,19 @@ bigobenchmark <- function(...,
 
 #' Autoplot for bigobenchmark object
 #'
-#' @param object
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 geom_line
+#' @importFrom ggplot2 geom_pointrange
 #'
-#' @return
-#' @export
+#' @param object
+#' @name autoplot
+#' @export autoplot.bigobenchmark
 #'
 #' @examples
+#' # Create plot for benchmarks
+#' library(ggplot2)
+#' bench <- bigobenchmark(1:n, for(i in 1:n) for(i in 1:n) 1:n, args=seq(from=1, to=100, length.out = 50))
+#' autoplot(bench)
 #'
 #' @author Andrew Prokhorenkov
 autoplot.bigobenchmark <- function(object) {
